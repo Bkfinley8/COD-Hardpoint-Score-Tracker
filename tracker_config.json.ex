93a7d5c8a66d82a1e69a5e311f0a33d70568219e
Bkfinley8 <brennan8192@gmail.com>
2025-05-18 00:55:01 -0400

    Automated

---
+++ tracker_config.json.ex
@@ -0,0 +1,40 @@
+{
+    "map_name": "Hacienda",
+    "map_number": 4,
+    "hill_duration": 60,
+    "team1": {
+      "name": "Optic Gaming",
+      "color": "#00FF00"
+    },
+    "team2": {
+      "name": "FaZe Clan",
+      "color": "#FF0000"
+    },
+    "map_settings": {
+        "Rewind" : {
+            "hill_duration": 60,
+            "rotation_length": 5
+        },
+        "Red Card" : {
+            "hill_duration": 60,
+            "rotation_length": 5
+        },
+        "Protocol" : {
+            "hill_duration": 60,
+            "rotation_length": 4
+        },
+        "Skyline" : {
+            "hill_duration": 60,
+            "rotation_length": 4
+        },
+        "Vault" : {
+            "hill_duration": 60,
+            "rotation_length": 4
+        },
+        "Hacienda" : {
+            "hill_duration": 60,
+            "rotation_length": 4
+        }
+    }
+  }
+  
